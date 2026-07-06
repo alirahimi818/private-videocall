869036cab1af6cd37f086b5f0eefc8f4712226d6
ali rahimi <alirahimi818@gmail.com>
2026-07-06 19:16:06 +0200

    feat(coturn): add TURN server config for the second-IP TLS listener
    Templated turnserver.conf rendered via envsubst at container start,
    use-auth-secret shared with node-service, RFC1918 peer denials, and
    quotas sized for a couple of concurrent calls.

---
+++ coturn/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+
+: "${TURN_SECOND_IP:?TURN_SECOND_IP env var is required}"
+: "${TURN_SHARED_SECRET:?TURN_SHARED_SECRET env var is required}"
+: "${TURN_REALM:?TURN_REALM env var is required}"
+: "${TURN_MIN_PORT:=49160}"
+: "${TURN_MAX_PORT:=49200}"
+
+export TURN_SECOND_IP TURN_SHARED_SECRET TURN_REALM TURN_MIN_PORT TURN_MAX_PORT
+
+envsubst < /etc/coturn/turnserver.conf.template > /etc/coturn/turnserver.conf
+
+exec turnserver -c /etc/coturn/turnserver.conf --no-daemon
